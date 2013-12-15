c50178a45989b115429c90824ac17356762cd24c
Kevin <Kevin@Kevin-PC>
2013-12-15 17:25:40 +0800

    Ant-HTML report

--- TEST1/src/TEST1.java
+++ TEST1/src/TEST1.java
@@ -305,7 +305,8 @@ public class TEST1{
 		 System.out.print("請輸入城市:");
 		 
 		 //使用者輸入要搜尋的城市
-		 String City="台北市";//buf.readLine();
+		 String City="高雄市";
+		 //String City=buf.readLine();
 		
 		 TEST1 test = new TEST1();
 		 Foodinfo fi = test.getFoodinfo();
